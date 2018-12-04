67b08a384b0689c2146c45fbd10748eb0b15530c
BrinyCoast <36340390+BrinyCoast@users.noreply.github.com>
2018-12-03 20:58:14 -0500

    javascript stuff

---
+++ OoGle-people-manager.js
@@ -0,0 +1,55 @@
+// The API toolkit for making REST systems easily
+// The API toolkit for making REST systems easily
+const express = require('express');
+// A good solution for handling JSON data in routes
+const bodyParser = require('body-parser');
+// Node JS modules for filesystem access
+const fs = require('fs');
+// Our database connection
+// This will be a JSON object of our programmers
+// and can be accessed as if it was any other javascript
+// object
+const database = require('./programmers.json');
+// Make an instance of our express application
+const app = express();
+// Specify our > 1024 port to run on
+const port = 3000;
+
+// Apply our middleware so our code can natively handle JSON easily
+app.use(bodyParser.json());
+// We must have our list of programmers to use
+if (!fs.existsSync('./programmers.json')) {
+  throw new Error('Could not find database of programmers!');
+}
+
+// Build our routes
+
+app.get('/', (req, res) => {
+  res.send(database);
+});
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+ 
+  res.send(database[req.params.id]);
+});
+
+app.put('/:id', (req, res) => {
+  const id = req.params.id;
+  database[id] = console.log(req.body);
+  res.send(database[req.params.id]);
+});
+
+app.post('/', (req, res) => {
+  const body = req.body; // Hold your JSON in here!
+  console.log(req.body);
+  database[req.body.name] = req.body
+  console.log(database)
+  res.send(200);
+});
+
+// IMPLEMENT A ROUTE TO HANDLE ALL OTHER ROUTES AND RETURN AN ERROR MESSAGE
+
+app.listen(port, () => {
+  console.log(`She's alive on port ${port}`);
+});
